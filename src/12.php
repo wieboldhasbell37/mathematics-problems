ca845d8b95499bdc0e39cfce77b16b30424398bf
wieboldhasbell37 <wieboldhasbell37@gmail.com>
2025-03-18 22:46:32 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$randomNumber1 = rand(1, 10);
+$randomNumber2 = rand(1, 10);
+$sum = $randomNumber1 + $randomNumber2;
+
+echo "The sum of $randomNumber1 and $randomNumber2 is $sum.";
+
+?>
