45476851cda6bfbd0d98b4dd04a632b5cc26ca4b
wieboldhasbell37 <wieboldhasbell37@gmail.com>
2025-04-07 03:34:59 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,13 @@
+<?php
+$number = 5;
+echo $number * 2; // Example of multiplying a number by 2
+
+// Another example: adding two numbers
+$first_number = 10;
+$second_number = 5;
+$sum = $first_number + $second_number;
+echo "The sum is: $sum";
+
+// A simple echo statement to print some text
+echo "Hello, World!";
+?>
