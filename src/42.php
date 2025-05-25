10a3d84573655a4218775d900622717714e43a3e
wieboldhasbell37 <wieboldhasbell37@gmail.com>
2025-05-25 10:51:35 +0000

    Add 42.php

---
+++ src/42.php
@@ -0,0 +1,30 @@
+<?php
+// Example PHP code to demonstrate basic arithmetic operations
+$a = 5;
+$b = 3;
+$c = 2;
+
+// Addition
+$sum = $a + $b;
+
+// Subtraction
+$difference = $c - $b;
+
+// Multiplication
+$product = $a * $b;
+
+// Division
+$qtr = $a / $b;
+
+// Modulus (remainder)
+$rmod = $a % $b;
+
+// Exponentiation
+$a_exp = pow($a, 2);
+
+// If statement example
+if ($a < 0) {
+    echo "The number is negative.";
+}
+
+echo "Sum: $sum\nDifference: $difference\nProduct: $product\nQuotient: $qtr";
